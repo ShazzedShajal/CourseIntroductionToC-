925052592ac18db8dc77e7b9e5c115981e584353
S.M. Shazzed Hossain Shajal <84393591+ShazzedShajal@users.noreply.github.com>
2026-08-06 22:46:00 +0600

    Add files via upload

---
+++ Greater of two Numbers using if else.cpp
@@ -0,0 +1,19 @@
+// Greater of two Numbers
+#include<iostream>
+using namespace std;
+
+ int main()
+{
+    int a,b;
+    cout<<"Enter two Numbers, Please:";
+    cin>>a>>b;
+    if(a>=b){
+        cout<<a<<" is Greater"<<endl;
+    }
+    else
+    {
+         cout<<b<<" is Greater"<<endl;
+    }
+
+    return 0;
+}
