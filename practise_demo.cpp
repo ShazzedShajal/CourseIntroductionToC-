26e20d8ca578ec91748e72f2e2d2aa8f5db878eb
Shazzed Hossain Shajal <shazzedshajal@gmail.com>
2026-09-03 08:40:57 +0600

    n

---
+++ practise_demo.cpp
@@ -0,0 +1,17 @@
+// Take an integer N and print all numbers from 1 to N
+//using while loop
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{ int n,i;
+    cout<<"ENter value of N: ";
+    cin>> n;
+
+    i= 1;
+    while(i<=n){
+        cout<<i<<" " ;
+        i++;
+    }
+
+   return 0;
+}
